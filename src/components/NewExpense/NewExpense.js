6f65cf82a91e932724bf1c49c06350a7c2fca5b4
Mohammad <atufi@outlook.com>
2022-09-28 16:26:55 +0330

    add new expense to state

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,7 +2,7 @@ import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
 export default function NewExpense(props) {
     const saveDataHandler = (newExpense) => {
-        props.addNewExpense ( {...newExpense, id:Math.random().toString() });
+        props.addNewExpense ( {...newExpense, id:Math.random().toString(),date: new Date(newExpense.date) });
     }
     return (
         <div className='new-expense'>
